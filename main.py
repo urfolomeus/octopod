264b8d61f1e9476d8acee3c253483149d79f593a
Alan Gardner <urfolomeus@gmail.com>
2022-08-17 23:35:12 +0100

    Extract a data fetcher

--- main.py
+++ main.py
@@ -1,22 +1,13 @@
 import json
-import os
 import pandas as pd
-import requests
-from requests.auth import HTTPBasicAuth
 
+from src.DataFetcher import DataFetcher
 
-BASE_URL = "https://api.octopus.energy/v1"
-MPAN = os.environ["OCTOPUS_MPAN"]
-SERIAL_NUMBER_ELEC = os.environ["OCTOPUS_SERIAL_ELEC"]
-PAGE_SIZE = 1000
+INSTALL_DATE = "2022-08-02T08:00:00"
 
-url = f"{BASE_URL}/electricity-meter-points/{MPAN}/meters/{SERIAL_NUMBER_ELEC}/consumption?page_size={PAGE_SIZE}"
+start_date = INSTALL_DATE
 
-# Octopus just provide an API token and expect that to be provided as a Basic Auth user
-# HTTPBasicAuth expects a password too, so we just use an empty string
-basic = HTTPBasicAuth(os.environ["OCTOPUS_TOKEN"], "")
-
-response = requests.request("GET", url, auth=basic)
+response = DataFetcher().fetch_electricity(start_date)
 
 results = json.loads(response.text)["results"]
 
---
+++ src/DataFetcher.py
@@ -0,0 +1,19 @@
+import os
+import requests
+
+BASE_URL = "https://api.octopus.energy/v1"
+PAGE_SIZE = 1000
+
+
+class DataFetcher:
+  def fetch_electricity(self, start_date):
+    mpan = os.environ["OCTOPUS_MPAN"]
+    serial = os.environ["OCTOPUS_SERIAL_ELEC"]
+    url = f"{BASE_URL}/electricity-meter-points/{mpan}/meters/{serial}/consumption?period_from={start_date}&page_size={PAGE_SIZE}"
+
+    return requests.request("GET", url, auth=self.__basic())
+
+  def __basic(self):
+    # Octopus just provide an API token and expect that to be provided as a Basic Auth user
+    # HTTPBasicAuth expects a password too, so we just use an empty string
+    return requests.auth.HTTPBasicAuth(os.environ["OCTOPUS_TOKEN"], "")
