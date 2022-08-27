1312b21e2d57a793c1fbb2bc1e55ea7aa621fdfa
Alan Gardner <urfolomeus@gmail.com>
2022-08-27 08:29:33 +0100

    Read consumption without recording

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 
 from src.connection import Connection
@@ -8,27 +9,31 @@ db = Connection()
 
 def record_consumption(consumption_type):
     last_recorded_date = db.get_last_recorded_date(consumption_type)
-    print(f"Fetching data recorded since {last_recorded_date}...")
+    print(f"Fetching {consumption_type} data recorded since {last_recorded_date}...")
+
     response = DataFetcher().fetch(consumption_type, last_recorded_date)
     results = json.loads(response.text)["results"]
     print(f"Received {len(results)} readings\n")
+
     if len(results):
         db.write(results, consumption_type)
 
 
 def read_consumption(consumption_type):
+    print(consumption_type)
+    print("-" * len(consumption_type))
     readings = db.read(consumption_type)
     print(readings.sort_values("interval_start", ascending=False))
 
 
-print("Electricity")
-print("-----------")
-record_consumption("elec")
+parser = argparse.ArgumentParser()
+parser.add_argument("-r", "--record", action="store_true", help="record new values")
+args = parser.parse_args()
+
+if args.record:
+    record_consumption("elec")
+    record_consumption("gas")
+
 read_consumption("elec")
-
 print("\n\n")
-
-print("Gas")
-print("---")
-record_consumption("gas")
 read_consumption("gas")
